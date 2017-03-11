746b1e294d0fdbe37461d1e1276efed1e3cee288
Aleksandr <luchcko.a@gmail.com>
2017-03-11 19:10:11 +0300

    add element to store

--- src/app/app.component.jsx
+++ src/app/app.component.jsx
@@ -1,13 +1,44 @@
 "use strict";
 
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 import './app.component.sass';
 
-export class App extends React.Component {
+class App extends Component {
+  addElement() {
+    this.props.addElement('test');
+  }
+
   render() {
+    console.log(this.props.testStore);
     return (
-      <h1>Hello World!</h1>
-    )
+      <div>
+        <h1>Hello World!</h1>
+        <button onClick={this.addElement.bind(this)}>add</button>
+        <ul>
+          {this.props.testStore.map((item, index) =>
+            <li key={index}>{item}</li>
+          )}
+        </ul>
+      </div>
+    );
   }
 }
+
+function mapState(state) {
+  return {
+    testStore: state
+  }
+}
+
+function dispatchState(dispatch) {
+  return {
+    addElement: function(elem) {
+      dispatch({type: 'ADD_ELEMENT', payload: elem});
+    }
+  }
+}
+
+const AppConnect = connect(mapState, dispatchState)(App);
+export default AppConnect;
--- src/main.js
+++ src/main.js
@@ -2,10 +2,31 @@
 
 import ReactDOM from 'react-dom';
 import React from 'react';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 
 import './assets/sass/index.sass';
 
-import {App} from './app/app.component';
-console.log('test');
+import App from './app/app.component';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = [
+  'element 1',
+  'element 2'
+];
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_ELEMENT':
+      return [...state, action.payload];
+  }
+  return state;
+}
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
