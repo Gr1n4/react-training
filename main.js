8b17e406ffe2dcfadbb0eb8536baf60282789e8c
Aleksandr <luchcko.a@gmail.com>
2017-03-11 03:22:24 +0300

    click button

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 "use strict";
 
+const testData = [
+  {
+    title: 'element 1',
+    id: 1
+  },
+  {
+    title: 'element 2',
+    id: 2
+  },
+  {
+    title: 'element 3',
+    id: 3
+  }
+];
+
 class App extends React.Component {
   render() {
     return (
       <div className="app">
         app component
-        <Child />
+        <Child data={testData}/>
+        <TestButton></TestButton>
       </div>
     );
   }
@@ -13,11 +29,32 @@ class App extends React.Component {
 
 class Child extends React.Component {
   render() {
+    let template = this.props.data.map((item, index) => {
+      return (
+        <div key={item.id}>
+          <p>{item.id} {item.title}</p>
+        </div>
+      )
+    });
+    console.log(template);
     return (
-      <div className="app">
+      <div className="child">
         child app component
+        {template}
       </div>
     );
+  }
+}
+
+class TestButton extends React.Component {
+  testClick() {
+    console.log('test');
+  }
+
+  render() {
+    return (
+      <button type="button" onClick={this.testClick}>test</button>
+    )
   }
 }
 
